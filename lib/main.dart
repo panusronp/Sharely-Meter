a989cce0599f87f669fff1d36f16bca781c2dce1
nrp <naruhpak.210@mail.kmutt.ac.th>
2020-08-19 23:13:48 +0700

    Add welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:sharelymeter/screens/welcome/welcome_screen.dart';
+import 'package:sharelymeter/constants.dart';
+
 import 'package:sharelymeter/screens/home/home_screen.dart';
 import 'package:sharelymeter/prematching/map.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,10 +16,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sharely Meter',
-      home: Home(),
-      // home: MapSample(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home:  WelcomeScreen(),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sharely Meter',
+//       home: Home(),
+//       // home: MapSample(),
+//     );
+//   }
+// }
 
