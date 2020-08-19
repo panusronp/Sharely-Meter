015f147e16de75616b522932846a57890cb4ea0b
Panusron <panusronphansod@gmail.com>
2020-08-19 18:57:07 +0700

    sub folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:sharelymeter/screens/home.dart';
-import 'package:sharelymeter/screens/show_map.dart';
+import 'package:sharelymeter/home/home.dart';
+import 'package:sharelymeter/home/map.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main(){
